70e863a4e4c21cc8f7c8991ce4863251407b6a6c
unknown <parrenoemmanuel756@gmail.com>
2022-11-30 05:58:14 +0800

    Fix the passwords do not match functionality

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const password = document.querySelector('#pass')
 const password2 = document.querySelector('#pass2')
-password2.addEventListener('change', ()=>{
+const span = document.querySelector('span')
+password2.addEventListener('keyup', ()=>{
     if (password.value == password2.value) {
-        password2.setAttribute('style', 'background-color:green')
+        password2.setAttribute('style', 'background-color:green');
+        span.textContent = '';
     } else {
-        password2.setAttribute('style', 'background-color:red')
+        password2.setAttribute('style', 'background-color:red');
+        span.textContent = '*Passwords don\'t match';
     }
 })
